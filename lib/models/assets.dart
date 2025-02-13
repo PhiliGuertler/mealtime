7c3585a0c92b6cdfb450e116680f178adf2937ff
Philipp Guertler <phili.gue@hotmail.de>
2025-02-13 22:09:05 +0100

    Add image assets

---
+++ lib/models/assets.dart
@@ -0,0 +1,13 @@
+enum ImageAssets {
+  calendar(value: 'assets/calendar.jpg'),
+  ingredients(value: 'assets/ingredients.jpg'),
+  intolerance(value: 'assets/intolerance.jpg'),
+  recipe(value: 'assets/recipe.jpg'),
+  search(value: 'assets/search.jpg'),
+  settings(value: 'assets/settings.jpg'),
+  ;
+
+  final String value;
+
+  const ImageAssets({required this.value});
+}
