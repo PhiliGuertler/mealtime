c0a932cc7f8535dbd0a7145bc2ae0ceb3a50488c
Philipp Guertler <phili.gue@hotmail.de>
2025-03-04 20:41:32 +0100

    Upgrade flutter and add a mock file for the database

--- lib/providers/database/database_file_provider.dart
+++ lib/providers/database/database_file_provider.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 
-import 'package:mealtime/providers/file_provider.dart';
+import 'package:flutter/services.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:path_provider/path_provider.dart';
 
 part 'database_file_provider.g.dart';
 
@@ -10,7 +11,19 @@ const String databaseFileName = "mealtime-store.json";
 
 @riverpod
 FutureOr<File> databaseFile(Ref ref) async {
-  final fileUtils = ref.watch(fileUtilsProvider);
-  final file = await fileUtils.openFile(databaseFileName);
+  // final fileUtils = ref.watch(fileUtilsProvider);
+  // final file = await fileUtils.openFile(databaseFileName);
+  // return file;
+
+  final fakeDatabaseFile = await ref.watch(fakeDatabaseFileProvider.future);
+  return fakeDatabaseFile;
+}
+
+@riverpod
+FutureOr<File> fakeDatabaseFile(Ref ref) async {
+  final byteData = await rootBundle.load("test_resources/mealtime-store.json");
+  final tempDir = await getTemporaryDirectory();
+  final file = File('${tempDir.path}/mealtime-store.json');
+  await file.writeAsBytes(byteData.buffer.asUint8List());
   return file;
 }
